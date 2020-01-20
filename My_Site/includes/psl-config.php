9e06b1992048dcdf02ccfb5a713ae8b409876edf
Robert Weinber <robert.weinber@gmail.com>
2020-01-20 22:29:28 +0100

    cleanup

--- My_Site/includes/psl-config.php
+++ My_Site/includes/psl-config.php
@@ -1,21 +1,15 @@
 ﻿<?php
 /**
  * These are the database login details
- */  
-define("HOST", "us-cdbr-azure-southcentral-f.cloudapp.net");     // The host you want to connect to.
-define("PORT", "3306");
-define("USER", "bebf66759dec3f");    // The database username. 
-define("PASSWORD", "ade6a46a");    // The database password. 
-define("DATABASE", "db-d5d4d1be-9e9a");    // The database name.
-/*
-define("HOST", "sql7.freemysqlhosting.net");     // The host you want to connect to.
-define("USER", "sql7136227");    // The database username. 
-define("PASSWORD", "Ls5mQiV9sZ");    // The database password. 
-define("DATABASE", "sql7136227");    // The database name.
  */
- 
+define("HOST", "asd");     // The host you want to connect to.
+define("PORT", "123");
+define("USER", "asd");    // The database username.
+define("PASSWORD", "asd");    // The database password.
+define("DATABASE", "asd");    // The database name.
+
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
- 
+
 define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
 ?>
